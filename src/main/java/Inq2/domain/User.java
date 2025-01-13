b82511d9ce09d9e0509ba3272b24d50b12bcf467
kyu-0907 <123appp@kyonggi.ac.kr>
2025-01-13 16:11:41 +0900

    a

--- src/main/java/Inq2/domain/User.java
+++ src/main/java/Inq2/domain/User.java
@@ -22,6 +22,11 @@ public class User {
     @Column(name="id", updatable = false)
     private Long id;
 
+    private Long ids;
+
+
+
+
     @Column(name = "username", nullable = false, unique = true)
     private String username;
 
